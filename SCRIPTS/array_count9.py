257c37edee07eb696deb506ff72db71c215d9fb9
Camilo <camilocastelar3@gmail.com>
2023-08-10 11:30:29 -0500

    practica 2

--- SCRIPTS/array_count9.py
+++ SCRIPTS/array_count9.py
@@ -6,11 +6,19 @@ array_count9([1, 9, 9]) → 2
 array_count9([1, 9, 9, 3, 9]) → 3
 """
 
+# Inicializamos la funcion
 def array_count9(nums):
-  count = 0
-  for i in nums:
-    if i == 9:
-      count += 1
-  return count
+    # Inicializamos el contador
+    count = 0
 
-print(array_count9([1, 2, 9]))
+    # Realizaomos un bucle que recorra el array
+    for i in nums:
+        # Realizamos la condición que identifica si el array conetiene 9
+        if i == 9:
+            # Si la condición aplica el contador sube 1 por cada 9
+            count+=1
+    # Retornamos el contador
+    return count
+
+# Corremos funcion
+print(array_count9([1, 2, 9, 99, 9]))
