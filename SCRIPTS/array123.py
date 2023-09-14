a36906b4ba4e485044a76e43fb1a8306bf7304c7
Camilo <camilocastelar3@gmail.com>
2023-09-14 17:17:53 -0500

    array123 recap

--- SCRIPTS/array123.py
+++ SCRIPTS/array123.py
@@ -7,12 +7,13 @@ array123([1, 1, 2, 1, 2, 3]) → True
 """
 
 def array123(nums):
-  # Note: iterate with length-2, so can use i+1 and i+2 in the loop
+  # Nota: iteracion length-2, para usar i+1 y i+2 en el loop
   for i in range(len(nums)-2):
     if nums[i] == 1 and nums[i+1] == 2 and nums[i+2] == 3:
       return True
   return False
 
+print(array123([1, 1, 1]))
 """
 array123([1, 1, 2, 3, 1]) → True
 array123([1, 1, 2, 4, 1]) → False
